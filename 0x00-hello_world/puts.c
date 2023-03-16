d942fc616a84f8dffaffd24259d8418bc341b6b4
Jareeyah <jariatdosunmu@gmail.com>
2023-03-16 14:13:36 -0700

    puts

---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A program using puts
+ *
+ * Return: Always 1 (Success)
+ */
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+		return (0)
+}
+
