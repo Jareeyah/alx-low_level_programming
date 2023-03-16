495c54369e70fde59e4970ff1d356adfa4b8dc07
Jareeyah <jariatdosunmu@gmail.com>
2023-03-16 15:46:21 -0700

    keys

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - Running a program using printf
  *
- * Return 1 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 
