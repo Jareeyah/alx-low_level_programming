975e51fb1aac52605643b0d4e9009ad7e69e2687
Jareeyah <jariatdosunmu@gmail.com>
2023-03-16 17:55:26 -0700

    finally

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Running a program
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 64);
+	return (1);
+}
