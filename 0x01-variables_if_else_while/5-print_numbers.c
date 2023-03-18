5652b61635e550aa673aea85039bd6eef1ef985b
Jareeyah <jariatdosunmu@gmail.com>
2023-03-18 10:51:59 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - printing all single digit numbers
+ *
+ * Return: Always 1 success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d\n", n);
+	return (0);
+}
+
