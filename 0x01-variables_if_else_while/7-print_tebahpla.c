9411de741bdefdaf9bdc55b1b34ab0b45f483a11
Jareeyah <jariatdosunmu@gmail.com>
2023-03-18 11:57:22 -0700

    print in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets in reverse
+ *
+ * Return: always 0 success
+ */
+int main(void)
+{
+	char k = 'z';
+
+	while (k <= 'a')
+{
+	putchar(k);
+}
+	putchar('\n');
+	return (0);
+}
