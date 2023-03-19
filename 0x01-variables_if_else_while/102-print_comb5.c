ef364f7a9a45c4d81749af8d67ca9861c0bde6a3
Jareeyah <jariatdosunmu@gmail.com>
2023-03-18 20:17:54 -0700

    alx task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,15 +10,15 @@ int main(void)
 	int m;
 	int n;
 
-	for (m = 0 ; m < 100 ; m++)
-		for (n = 0 ; n < 100 ; n++)
+	for (m = 0 ; m <= 98 ; m++)
+		for (n = m + 1 ; n <= 99 ; n++)
 	{
 			putchar((m / 10) + '0');
 			putchar((m % 10) + '0');
 			putchar(' ');
 			putchar((n / 10) + '0');
 			putchar((n % 10) + '0');
-		if (m != 99 || n != 99)
+		if (m != 98 || n != 99)
 		{
 			putchar(',');
 			putchar(' ');
