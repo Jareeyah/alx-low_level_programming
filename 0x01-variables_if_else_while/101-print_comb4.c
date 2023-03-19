a17b144cbf225aa730390783b79d8308734df162
Jareeyah <jariatdosunmu@gmail.com>
2023-03-18 18:57:31 -0700

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - combinations of three digits
+ *
+ * Return: always 0 success
+ */
+int main(void)
+{
+	int m;
+	int n;
+	int o;
+
+for (m = 0 ; m < 10 ; m++)
+{
+	for (n = m + 1 ; n < 10 ; n++)
+	{
+		for (o = n + 1 ; o < 10 ; o++)
+		{
+			putchar(m + '0');
+			putchar(n + '0');
+			putchar(o + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
+
+putchar('\n');
+return (0);
+}
+
