70ab498ab46467afad1c6aa17211c67a2c0b9723
Jareeyah <jariatdosunmu@gmail.com>
2023-03-21 13:55:58 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet - a function that prints lowecase alphaets
+ *
+ * Return: always 0 success
+ */
+void print_alphabet(void)
+{
+	char m = 'a';
+
+	for (m < 'z')
+		_putchar(m);
+	m++;
+		_putchar('\n');
+		return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char m);
+void print_alphabet(void);
 
 
 #endif
