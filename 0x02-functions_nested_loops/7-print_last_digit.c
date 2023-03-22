ddbec93f76d6840e3f7accee73d9d7a198336ac4
Jareeyah <jariatdosunmu@gmail.com>
2023-03-21 20:11:55 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
 /**
  * print_last_digit - print last digit of a number
  * @k: char to be checked
- * Return: value of last digit
+ * Return: value of the last digit
  */
 int print_last_digit(int k)
 {
@@ -12,14 +12,14 @@ int print_last_digit(int k)
 	if (k < 0)
 	{
 		k = -k;
-	l = k % 10;
 	}
+	l = k % 10;
 
 	if (l < 0)
 	{
 		l = -l;
-		_putchar(l + '0');
 	}
+		_putchar(l + '0');
 	return (l);
 }
 
