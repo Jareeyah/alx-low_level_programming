0a429ce2c5e4debb7b6183f86bb9774247102bd8
Jareeyah <jariatdosunmu@gmail.com>
2023-03-22 13:38:28 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two integer
+ * @m: is int to be added to n
+ * @n: is int to be added to m
+ * Return: the result
+ */
+int add(int m, int n)
+{
+
+	_putchar(m && n);
+	return (m + n);
+}
+
+
