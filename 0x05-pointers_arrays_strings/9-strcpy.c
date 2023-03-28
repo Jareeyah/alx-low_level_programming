69a39507579151233ca6968cdf99280d6a06d245
Jareeyah <jariatdosunmu@gmail.com>
2023-03-28 15:20:24 -0700

    9-strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copies string
+ * @dest: char to pointer to be checked
+ * @src: char to pointer to be checked
+ * Return: to the pointer dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int p = 0;
+
+	while (src[p] != '\0')
+	{
+		dest[p] = src[p];
+		p++;
+	}
+		dest[p] = '\0';
+	return (dest);
+}
