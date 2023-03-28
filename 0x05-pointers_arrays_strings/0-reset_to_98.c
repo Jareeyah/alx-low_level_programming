c2337e86d3cce11c166e730bfdaad6b0c5d5debe
Jareeyah <jariatdosunmu@gmail.com>
2023-03-27 17:17:23 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - fuction of int to point to 98
+ * @*n: pointer to be checked
+ * @n: int to be checked
+ */
+void reset_to_98(int *n)
+{
+	int m = 98;
+	*n = m;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char i);
+void reset_to_98(int *n);
+
+
+
+#endif
