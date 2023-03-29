4bdc3254c5dd38ddb89989684311eed21905073e
Jareeyah <jariatdosunmu@gmail.com>
2023-03-28 17:11:00 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,23 +2,27 @@
 
 /**
  * _atoi - function that converts string to integer
- * @s: string to pointer to be checked
+ * @s: string to be converted
  * Return: int value of the converted string
  */
 int _atoi(char *s)
 {
 	int r = 1;
-	int p = 0;
+	unsigned int p = 0;
 
 	do {
 		if (*s == '-')
 			r *= -1;
+
+		else if (*s == '+')
+			r *= -1;
+
 		else if (*s >= '0' && *s <= '9')
 			p = (p * 10) + (*s - '0');
+
 		else if (p > 0)
 			break;
-		s++;
-	} while (*s != '\0');
+	} while (*s++);
 	return (r * p);
 }
 
