e18cc7216eef8b762543c06525286d72947aaf8f
Jareeyah <jariatdosunmu@gmail.com>
2023-05-12 13:01:16 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int c;
-	int t;
+	int t = 0;
 
 	if (filename == NULL)
 	{
@@ -18,7 +18,10 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	for (t = 0 ; text_content[t] != '\0' ; t++)
+	while (text_content[t] != '\0')
+	{
+		t++;
+	}
 	c = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (c == -1)
 	{
